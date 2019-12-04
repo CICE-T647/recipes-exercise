cfb0817a5da147e1b964aa4d5612727c73ee5347
olatzaranzabe <olatzaranzabe@gmail.com>
2019-12-04 21:59:15 +0100

    wip delete recipe

--- app.js
+++ app.js
@@ -4,20 +4,27 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const recipesBuffer = fs.readFileSync("./recipes.json");
+
+let recipes;
+try {
+    recipes = JSON.parse(recipesBuffer);
+} catch (e) {
+    recipes = [];
+}
+
 app.get("/getrecipes", (req, res) => {
-    const recipesBuffer = fs.readFileSync("./recipes.json");
-
-    const recipes = JSON.parse(recipesBuffer);
     console.log("recipes", recipes);
-
-    res.status(200).json(recipes);
+    if (recipes) res.status(200).json(recipes);
+    res.status(404).json({ message: `Recipes not found` });
 });
 
 app.get("/getrecipesbyid", (req, res) => {
-    const { id, title } = req.query;
-    const recipesBuffer = fs.readFileSync("./recipes.json");
+    const { id } = req.query;
 
-    const recipes = JSON.parse(recipesBuffer);
     const recipeById = recipes.find(recipe => {
         if (recipe.id === id) {
             return id;
@@ -25,11 +32,25 @@ app.get("/getrecipesbyid", (req, res) => {
     });
     console.log("recipe", recipeById);
     console.log("recipe", id);
-
-    res.status(200).json(recipeById);
+    if (recipeById) {
+        res.status(200).json(recipeById);
+    } else {
+        res.status(404).json({ message: `Recipe with id: ${id} not found` });
+    }
 });
 
 app.put("/addnewrecipe", (req, res) => {
+    const {
+        title,
+        level,
+        ingredients,
+        cuisine,
+        dishType,
+        image,
+        duration,
+        creator
+    } = req.body;
+
     const newRecipe = {
         id: recipes.length + 1,
         title,
@@ -41,7 +62,65 @@ app.put("/addnewrecipe", (req, res) => {
         duration,
         creator
     };
-    recipes.push(newRecipe);
+
+    console.log(newRecipe);
+    try {
+        recipes.push(newRecipe);
+        fs.writeFileSync("./data.json", JSON.stringify(recipes, null, 4));
+        res.status(200).json(recipes);
+    } catch (err) {
+        res.status(404).json({ message: "page not found" });
+    }
+});
+
+app.post("/updaterecipe/:id", (req, res) => {
+    const { id } = req.params;
+    const {
+        title,
+        level,
+        ingredients,
+        cuisine,
+        dishType,
+        image,
+        duration,
+        creator
+    } = req.body;
+    console.log(req.body);
+    console.log(id);
+    const updatedRecipies = recipes.map(recipe => {
+        if (recipe) {
+            if (recipe.id === id) {
+                recipe.title = title ? title : recipe.title;
+                recipe.level = level ? level : recipe.level;
+                recipe.cuisine = cuisine ? cuisine : recipe.cuisine;
+                recipe.ingredients = ingredients
+                    ? ingredients
+                    : recipe.ingredients;
+                recipe.dishType = dishType ? dishType : recipe.dishType;
+                recipe.image = image ? image : recipe.image;
+                recipe.duration = duration ? duration : recipe.duration;
+                recipe.creator = creator ? creator : recipe.creator;
+            }
+        }
+    });
+    try {
+        res.status(200).json(updatedRecipies);
+    } catch (err) {
+        res.status(404).json({ message: "page not found" });
+    }
+});
+
+app.delete("/deleterecipe/:id", (req, res) => {
+    const { id } = req.params;
+    console.log("id", id);
+    // recipe[id - 1] = null;
+    const index = recipes.map(recipe => {
+        if (recipe.id === id) {
+            console.log(recipe.id);
+        }
+    });
+
+    res.status(200).json({ message: "usuario actualizado correctamente" });
 });
 
 app.listen(PORT, () => {
